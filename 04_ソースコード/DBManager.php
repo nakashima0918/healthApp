d8d51d00bee47c1b33d2523eef7a0d1be80d2742
shingo [ <115526343+nakashima0918@users.noreply.github.com>
2023-10-23 14:57:43 +0900

    データモデル設計書の更新

--- 04_ソースコード/DBManager.php
+++ 04_ソースコード/DBManager.php
@@ -3,7 +3,7 @@ class DBManager
 {
 
     // DB接続のメソッド
-    public function dbConnect() {
+    private function dbConnect() {
         $dsn = 'mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1418437-healthapp;charset=utf8';
         $user = 'LAA1418437';
         $password = 'fujihealth';
@@ -17,20 +17,14 @@ class DBManager
         $pdo = $this->dbConnect();
         $sql = "SELECT * FROM user";
         $ps = $pdo->query($sql);
+
         $ps->execute();
+
         $result = $ps->fetchAll();
         return $result;
     }
 
-    // テスト用
-    public function testDate() {
-        $pdo = $this->dbConnect();
-        $sql = "SELECT CURRENT_TIME";
-        $ps = $pdo->query($sql);
-        $ps->execute();
-        $result = $ps->fetchAll();
-        return $result;
-    }
+    //以下処理---------------------------
 
 
 }
