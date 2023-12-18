34913b93047eb533fb9b5e532c8cc771d65fea68
shingo [ <115526343+nakashima0918@users.noreply.github.com>
2023-12-18 10:35:36 +0900

    写真の追加

--- 04_ソースコード/3-8-reset_pass_adchk.php
+++ 04_ソースコード/3-8-reset_pass_adchk.php
@@ -5,17 +5,18 @@ $dbmng = new DBManager();
 
 // メアド未入力チェック
 if(empty($_POST['mail'])) {
-    $_SESSION['error'] = "パスワードが入力されていません。";
+    $_SESSION['error'] = "メールアドレスが入力されていません。";
     header('Location: 3-1-reset_pass_uchk.php');
 }else{
     $_SESSION['mail'] = $_POST['mail'];
     header('Location: 3-2-reset_pass.php');
 }
+
 //メアドが登録されているか
-$address = $_POST['address'];
+$address = $_POST['mail'];
 
 // データベースでメールアドレスが存在するか確認
-$sql = "SELECT * FROM user WHERE メールアドレス = '$address'";
+$sql = "SELECT * FROM user WHERE address = '$address'";
 $result = $conn->query($sql);
 
 // メールアドレスが存在しない場合はエラーを出力
